3a5ce140d5b263fbc6340185a808c230fce757e8
CodeITVadym-Bilobrytskyi <yanko.dmitriy@gmail.com>
2026-03-15 15:36:27 +0200

    create top bar component

--- apps/store/src/app/layout.tsx
+++ apps/store/src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { TopBar } from '../components/TopBar/TopBar';
+import { Body } from '../components/Body/Body';
 
 export const metadata: Metadata = {
   title: 'Storefront',
-  description: 'E-commerce storefront'
+  description: 'E-commerce storefront',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Body>
+        <TopBar />
+        {children}
+      </Body>
     </html>
   );
 }
---
+++ apps/store/src/components/TopBar/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TopBar = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-4">
+      <div className="md:max-w-5xl">My-Tangle - магазин оригинальных Tangle Teezer</div>
+    </div>
+  );
+};
